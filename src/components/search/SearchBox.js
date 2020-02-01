354c7035b7e165d2f56069932b8b62830b70cbd3
Isabelgpils@gmail.com <isabelgpils@gmail.com>
2020-02-01 15:29:44 -0600

    fixed typo

--- src/components/search/SearchBox.js
+++ src/components/search/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox = ({ searchfield, onSearchChange }) => {
 	return (
 		<div>
-			<input onChange={onSearchChange} type="searcy" placeholder="search threads" />
+			<input onChange={onSearchChange} type="search" placeholder="search threads" />
 		</div>
 	);
 };
